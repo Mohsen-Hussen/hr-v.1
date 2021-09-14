112839c4c7c83715c0f2dfd22a0b15290fd72602
Mohsen-Hussen <mohsenhussen8@gmail.com>
2021-09-14 17:46:47 +0300

    faq edit

--- js v.1/main.js
+++ js v.1/main.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
-    console.log("ready");
-    let chevron = document.getElementById('chevron');
-    console.log(chevron);
+    $('.iconBtn').click(function(){
+        let iconBtnClassName = $(this).attr("class");
+        if (iconBtnClassName.includes('collapsed')) {
+            console.log("i'm here");
+            $(this).children('i').removeClass("fa-chevron-down").addClass("fa-chevron-up");
+        } else {
+            console.log("failed");
+            $(this).children('i').removeClass("fa-chevron-up").addClass("fa-chevron-down");
+        }
+    });
 });
