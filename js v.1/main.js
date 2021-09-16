7c5eff76433999b8c75e4469cb7d19645a5daf9d
Mohsen-Hussen <mohsenhussen8@gmail.com>
2021-09-16 14:33:28 +0300

    add blog details page

--- js v.1/main.js
+++ js v.1/main.js
@@ -9,4 +9,33 @@ $(document).ready(() => {
             $(this).children('i').removeClass("fa-chevron-up").addClass("fa-chevron-down");
         }
     });
+
+    /*owl carousel activated*/
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        nav: true,
+        // lazyLoad:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            // 720:{
+            //     items:2
+            // },
+            // 960:{
+            //     items:3
+            // }
+        }
+    })
+    // $('.play').on('click',function(){
+    //     owl.trigger('play.owl.autoplay',[1000])
+    // })
+    // $('.stop').on('click',function(){
+    //     owl.trigger('stop.owl.autoplay')
+    // })
 });
